c78c5d51cfa71293650c9ebd06ff789d42008609
john-e-moore <jmoore87jr@gmail.com>
2024-04-17 19:32:24 -0400

    moved utils to bronze layer to keep deployment slim. wrote scraping logic and saving locally; games have already started so I am getting 404'd and need to try again tomorrow

--- src/layers/bronze/draftkings/mlb/main.py
+++ src/layers/bronze/draftkings/mlb/main.py
@@ -1,6 +1,6 @@
-import json, yaml, requests
+import json, yaml, requests, time, sys
 from datetime import datetime
-from common.s3_utils import S3Utility
+from layers.bronze.bronze_utils import BronzeUtility
 
 def load_yml(filepath: str) -> dict:
     """Load config from yml file."""
@@ -11,20 +11,62 @@ def load_yml(filepath: str) -> dict:
         print(f"Error in configuration file:\n{exc}")
 
 def main():
+
     # Load config
     config_path = "common/config.yml"
     config = load_yml(config_path)
-    bucket = config['aws']['bucket']
-    key = config['aws']['bronze_draftkings_s3_key']
-    url = config['webscraping']['draftkings']['url']
-    sleep_secs = config['webscraping']['draftkings']['sleep']
+    aws_config = config['aws'][0]
+    webscraping_config = config['webscraping']
+    bucket = aws_config['bucket']
+    key = aws_config['bronze_draftkings_s3_key']
+    url_template = webscraping_config[1]['draftkings']['url_template']
+    sleep_secs = webscraping_config[1]['draftkings']['sleep']
 
     # Load API schema
     api_path = "layers/bronze/draftkings/api.yml"
-    api = load_yml(api_path)
+    api_mlb = load_yml(api_path)['eventgroups'][0]
+    eventgroup_mlb = api_mlb['id']
 
-    # Request data
-    # Loop through categories and subcategories
+    # Request data for each subcategory
+    data_dir = webscraping_config[3]['data-directory']
+    headers = webscraping_config[0]['headers']
+    print(headers)
+    #sys.exit()
+    for category in api_mlb['categories']:
+        category_id = category['id']
+        print(f"Category name: {category['name']}")
+        for subcategory in category['subcategories']:
+            subcategory_id = subcategory['id']
+            subcategory_name = subcategory['name']
+            print(f"Subcategory name: {subcategory_name}")
+            # Construct URL
+            url = url_template.format(
+                eventgroup_id=eventgroup_mlb, 
+                category_id=category_id, 
+                subcategory_id=subcategory_id)
+            print(f"URL: {url}")
+            print(f"Headers: {headers}")
+            # Fetch data
+            try:
+                response = requests.get(
+                    url=url,
+                    headers=headers
+                )
+            except Exception as e:
+                print(response.status_code)
+                print(e)
+            # Write data
+            if response.status_code == 200:
+                timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+                with open(f'{data_dir}/{subcategory_name}_{timestamp}.json', 'w') as f:
+                    json.dump(response.text, f)
+            else:
+                print("Error fetching data.")
+            print("Sleeping for 3 seconds...")
+            time.sleep(3)
+
+    
+
     # Construct url
     # Request then sleeping for 3s
     
